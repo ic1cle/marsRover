f1a7aceacc18a435c0dda48822a12bf075cc9476
ic1cle <sigsos1@hotmail.com>
2023-12-19 14:08:49 +0100

    asda

--- marsRover.py
+++ marsRover.py
@@ -122,7 +122,7 @@ class Queue(list):
 		print('Current Turn Radius: ' + str(self.radius))
 		print('Current Cmd Duration: ' + str(self.duration) + " Seconds")
 		# print('PID Position: ' + roboclaw.ReadM2PositionPID(132))
-		print('ENC Val: ' + roboclaw.ReadEncM2(132))
+		print('ENC Val: ' + roboclaw.ReadEncM2(Roboclaw, 132))
 		print("------------------------------------------")
 
 	def run_next(self):
@@ -360,7 +360,7 @@ if __name__ == '__main__':
 		setup()
 		print('Setup completed with default ip')
 		# print('PID Position: ' + roboclaw.ReadM2PositionPID(132))
-		print('ENC Val: ' + roboclaw.ReadEncM2(132))
+		print('ENC Val: ' + roboclaw.ReadEncM2(Roboclaw, 132))
 	except:
 		retry_query = input('Setup failed. Do you want to...\n  1. Retry with different ip\n  2. Run local testing\n  3. Exit\nResponse: ')
 		while True:
